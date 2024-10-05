2ff0fef7b4e24951ec1593bdc08af1307cfe33a4
ganang andika kurnia putra <myinwork14@gmail.com>
2024-10-05 09:01:20 +0700

    jobsheet 6

--- Selection2exp108.java
+++ Selection2exp108.java
@@ -1,26 +1,28 @@
 import java.util.Scanner;
 public class Selection2exp108 {
     public static void main(String[] args) {
-        Scanner input08 = new Scanner(System.in);
+        Scanner input244107060008 = new Scanner(System.in);
         int year;
 
         System.out.print("input year : ");
-        year = input08.nextInt();
+        year = input244107060008.nextInt();
 
         if (year%4 == 0) 
-        {
-            if (year%100 != 0)
-            System.out.println("leap year");
-        else
+            if (year%100 == 0)
+                if (year%400 == 0)
+                    System.out.println("leap year");
+                else
+                    System.out.println("not leap year");
+
+            else
+                System.out.println("leap year");
+
+            else
             System.out.println("not leap year");
 
-        }
-        else
-        {
-            System.out.println("not leap year");
-        }
-
-
+        
+        
+        
     }
-} 
-    
+
+}
---
+++ bookshop.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class bookshop {
+
+    public static void main(String[] args) {
+        Scanner input08 = new Scanner(System.in);
+        String bookcategory, day;
+        int numberbooks;
+        double discount; 
+
+        System.out.print("input day : ");
+        day = input08.nextLine();
+
+        System.out.print("input book category : ");
+        bookcategory = input08.nextLine();
+        System.out.print("input number of books : ");
+        numberbooks = input08.nextInt();
+
+       if (bookcategory = dictionary )
+    }
+}
